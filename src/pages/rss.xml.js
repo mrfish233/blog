53c015089bf8b0b01f77e10b71f6c2f397870408
mrfish233 <baboom003@gmail.com>
2025-10-15 12:42:32 +0800

    feat: replace RSS feed generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,24 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+    const posts = await getCollection('blog')
+        .then((posts) => posts.sort((a, b) => b.data.pubDate - a.data.pubDate))
+        .then((posts) => {
+            console.log(posts);
+            return posts.map((post) => ({
+                title: post.data.title,
+                pubDate: post.data.pubDate,
+                description: post.data.description,
+                link: `/posts/${post.id}`,
+            }));
+        });
+
     return rss({
         title: 'Astro Learner | Blog',
         description: 'My journey learning Astro',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts,
         customData: `<language>en-us</language>`,
     });
 }
