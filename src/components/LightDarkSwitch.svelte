bbb0126e84fa39f1188b367f503c09089ab30c5c
mrfish233 <baboom003@gmail.com>
2026-03-16 18:34:14 +0800

    fix: suppress svelte type error

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -11,8 +11,6 @@ import {
 import { onMount } from "svelte";
 import type { LIGHT_DARK_MODE } from "@/types/config.ts";
 
-let {} = $props();
-
 const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE, AUTO_MODE];
 let mode: LIGHT_DARK_MODE = $state(AUTO_MODE);
 
